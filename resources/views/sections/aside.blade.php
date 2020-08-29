f5c3161b22eca5fee018b52bbd211e54fcb58a31
osbaldo <al221410832@gmail.com>
2020-08-29 11:11:04 -0500

    cambios en configuraciones de archivo .env y .gitignore para implementacion de plantilla

--- resources/views/sections/aside.blade.php
+++ resources/views/sections/aside.blade.php
@@ -2,7 +2,7 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="../../index3.html" class="brand-link">
-      <img src="AdminLTE/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+      <img src="AdminLTE/dist/img/AdminLTELogo.png" alt="App Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
       <span class="brand-text font-weight-light">APP</span>
     </a>
 
